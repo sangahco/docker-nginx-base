4f11d1a26b9b9e3b406bac438b68c4c061350145
Emanuele Disco <emawind84@gmail.com>
2017-08-21 17:21:37 +0900

    update

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -6,6 +6,6 @@ if [ "$NAMESERVERS" == "" ]; then
     export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 fi
 
-envsubst '$NAMESERVERS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$NAMESERVERS' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;" "$@"
